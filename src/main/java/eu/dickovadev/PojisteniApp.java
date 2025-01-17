a9d2c15c528d4a7a1611f151d78eb1a42d565f34
Karolína Dicková <kdickova.dev@gmail.com>
2025-01-17 15:27:36 +0100

    Initial commit

---
+++ src/main/java/eu/dickovadev/PojisteniApp.java
@@ -0,0 +1,13 @@
+package eu.dickovadev;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojisteniApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PojisteniApp.class, args);
+    }
+}
